3a2f3c744eb071f93469493f4ddcab302347a9f0
LOMBAYE <lombayedev@gmail.com>
2023-03-26 22:00:28 +0000

    details of a customer

--- src/App.js
+++ src/App.js
@@ -14,40 +14,24 @@ import Shared from "./components/Shared";
 import BankAccount from "./components/BankAccount";
 
 function App() {
-    return ( <
-        div className = "container-fluid"
-        id = "main" >
-        <
-        Header / >
-        <
-        BrowserRouter >
-        <
-        Routes >
-        <
-        Route path = "/mybank"
-        element = { < BankAccount / > } > < /Route> <
-        Route path = "/shared"
-        element = { < Shared / > } > < /Route> <
-        Route path = "/"
-        element = { < Customers / > } > < /Route> <
-        Route path = "/account"
-        element = { < Account / > } > < /Route> <
-        Route path = "/bankApp"
-        element = { < BankApp / > } > < /Route> <
-        Route path = "/customers"
-        element = { < Customers / > } > < /Route> <
-        Route path = "/addCustomer"
-        element = { < AddCustomer / > } > < /Route> <
-        Route path = "details/:id"
-        element = { < Details / > } > < /Route> <
-        Route path = "/newAccount/:customerId"
-        element = { < NewAccount / > } > < /Route> < /
-        Routes >
-        <
-        /BrowserRouter>
+    return (
+        <div className="container-fluid" id="main">
+            <Header/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/mybank" element={<BankAccount/>}></Route>
+                    <Route path="/shared" element={<Shared/>}></Route>
+                    <Route path="/"  element={ <Customers/>}></Route>
+                    <Route path="/account" element={<Account/>}></Route>
+                    <Route path="/bankApp" element={<BankApp/>}></Route>
+                    <Route path="/customers" element={ <Customers/>}></Route>
+                    <Route path="/addCustomer" element={<AddCustomer/>}></Route>
+                    <Route path="details/:id" element={ <Details/>}></Route>
+                    <Route path="/newAccount/:customerId" element={<NewAccount/>}></Route>
+                </Routes>
+            </BrowserRouter>
 
-        <
-        /div>
+        </div>
     );
 }
 
